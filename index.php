645d79fb94ae1ab3fca858730ce982b48be829f5
Gerard O'Neill <grardb@gmail.com>
2012-07-21 14:12:04 -0400

    Initial commit. Basic layout created

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Battle of the Braces</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/foundation.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/styles.css" />
+	</head>
+	<body>
+		<div class="row">
+			<header>
+				<h1>The Price is Wrong</h1>
+			</header>
+		</div>
+		<div class="row">
+			<div class="twelve columns">
+				<div class="row">
+					<div class="twelve columns">
+						<input type="text" name="title" id="title" placeholder="Title" />
+					</div>
+				</div>
+				<div class="row">
+					<div class="nine columns">
+						<img src="http://www.placehold.it/500x300" alt="" />
+					</div>
+					<div class="three columns">
+						<div class="row">
+							<div class="twelve columns">
+								<input type="text" name="price" id="price" placeholder="Price" />
+							</div>
+						</div>
+						<div class="row">
+							<div class="twelve columns">
+								<input type="button" name="finish_button" class="success button round large" id="finish-button" value="Finish" />
+							</div>
+						</div>
+						<div class="row">
+							<div class="twelve columns">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="twelve columns">
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+		<script src="assets/js/underscore.js"></script>
+		<script src="assets/js/backbone.js"></script>
+		<script src="assets/js/app.js"></script>
+	</body>
+</html>
