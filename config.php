7e9a25803d138bd31c377baa4e3f76a2527bb572
Gerard O'Neill <grardb@gmail.com>
2012-08-09 09:06:05 -0400

    config.php

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+define('ETSY_API_KEY', '');
+define('ETSY_ACTIVE_LISTINGS_URL', 'http://openapi.etsy.com/v2/listings/active?page=%d');
+define('ETSY_LISTING_IMAGES_URL', 'http://openapi.etsy.com/v2/listings/%d/images?');
